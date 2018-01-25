887f9a9c00b8d48f56beb965d3222628ed46c5ac
BilboTheGreedy <daniel.rapp@live.se>
2018-01-25 15:21:39 +0100

    add env var for listen ports

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 if [ -z "$CERT" ]
 then
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER -b
+  echo $1 |openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER -b
 else
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT -b
+  echo $1 |openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT --no-dtls -b
 fi
 
 sleep 5
+cat /etc/danted.conf | sed "s/\$PI/${$PI}/" /etc/danted.conf
 
 /usr/sbin/danted -f /etc/danted.conf -D
 
-socat -d TCP4-LISTEN:3389,fork TCP4:$JUMP1:3389 &
-socat -d TCP4-LISTEN:3390,fork TCP4:$JUMP2:3389 &
+socat -d TCP4-LISTEN:$LP1,fork TCP4:$JUMP1:3389 &
+socat -d TCP4-LISTEN:$LP2,fork TCP4:$JUMP2:3389 &
 
 /bin/bash
